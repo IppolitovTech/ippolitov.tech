04693da46ba85fb0c8ac32f88caf042458c388a1
k.ippolitov <k.ippolitov@niikmg.kz>
2022-05-05 16:41:29 +0600

    3 pages added

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col"><a href="/">Head</a></div>
+    <div class="col"><a href="portfolio">Portfolio</a></div>
+    <div class="col"><a href="contacts">Contacts</a></div>
+</div>
