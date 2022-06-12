5eff93af9b8adcd9339d753de552cee2085ead0c
k.ippolitov <k.ippolitov@niikmg.kz>
2022-06-13 05:28:52 +0600

    Crud for portfolio page

---
+++ resources/views/pages/portfolio/edit.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Edit</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Edit</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('portfolio.index') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('portfolio.update',$portfolio->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Img:</strong>
+                        <input type="text" name="img" value="{{ $portfolio->img }}" class="form-control" placeholder="Img">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Header:</strong>
+                        <input type="text" name="header" value="{{ $portfolio->header }}" class="form-control" placeholder="Header">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Text:</strong>
+                        <input type="text" name="text" value="{{ $portfolio->text }}" class="form-control" placeholder="Text">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Route::get('getAllLinks', 'MainMenuController@getAllLinks');
 
+Route::resource('portfolio', portfolioController::class);
 Route::get('/welcome', function () {
     return view('welcome');
 });
