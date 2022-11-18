bea38123afbddc56a3bff7136e7760f36d24d623
k.ippolitov <k.ippolitov@niikmg.kz>
2022-11-19 04:17:31 +0600

    hide menu on the public page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
             @yield('content')
         </div>
         @endisset
+        @if(!isset($title))
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -89,6 +90,7 @@
                 </div>
             </div>
         </nav>
+        @endif
 
         <main class="py-4">
             @yield('content')
