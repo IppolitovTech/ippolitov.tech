275905baf3042a3249e0bb528fd370aef7d10ba0
Konstantin Ippolitov <ippolitov.konstantin@gmail.com>
2023-09-27 09:05:56 +0600

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 </head>
 
 <body>
