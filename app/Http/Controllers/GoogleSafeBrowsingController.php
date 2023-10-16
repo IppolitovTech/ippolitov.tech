bb6bd9d50adc973e2077fda05c8cf45ec6e63dd3
Konstantin Ippolitov <ippolitov.konstantin@gmail.com>
2023-10-16 11:43:39 +0600

    fixed2

--- app/Http/Controllers/GoogleSafeBrowsingController.php
+++ app/Http/Controllers/GoogleSafeBrowsingController.php
@@ -4,51 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Google;
+use Google_Service_Indexing;
+use Google_Service_Indexing_UrlNotification;
 
 class GoogleSafeBrowsingController extends Controller
 {
-    private $apiKey;
-    private $clientId;
-
-    public function __construct()
-    {
-        $this->apiKey = env('GOOGLE_SAFE_BROWSING_API_KEY');
-        $this->clientId = env('GOOGLE_SAFE_BROWSING_CLIENT_ID');
-    }
-
-    public function scanLink(Request $request)
-    {
-        $url = $request->input('url');
-        $errors = $this->getThreats($url);
-
-        return empty($errors)
-            ? "The link is safe."
-            : "The link contains the following threats: " . implode(', ', $errors);
-    }
-
     public function submitForIndexing(Request $request)
     {
+        $apiKey = env('GOOGLE_SAFE_BROWSING_API_KEY');
+        $clientId = env('GOOGLE_SAFE_BROWSING_CLIENT_ID');
         $url = $request->input('url');
-        $errors = $this->getThreats($url);
 
-        return empty($errors)
-            ? "The URL has been submitted for indexing."
-            : "The link contains the following threats: " . implode(', ', $errors);
-    }
-
-    private function getThreats($url)
-    {
         $client = new Client();
 
-        $response = $client->post("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=$this->apiKey", [
+        $response = $client->post("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=$apiKey", [
             'json' => [
                 'client' => [
-                    'clientId' => $this->clientId,
+                    'clientId' => $clientId,
                     'clientVersion' => '1.0',
                 ],
                 'threatInfo' => [
-                    'threatTypes' => ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE', 'POTENTIALLY_HARMFUL_APPLICATION'],
-                    'platformTypes' => ['ANY_PLATFORM'],
                     'threatEntryTypes' => ['URL'],
                     'threatEntries' => [
                         ['url' => $url],
@@ -59,13 +35,47 @@ class GoogleSafeBrowsingController extends Controller
 
         $data = json_decode($response->getBody());
 
-        $errors = [];
-        if (isset($data->matches)) {
+        if ($this->isEmptyObject($data)) {
+
+            $indexingResponse = $this->submitToWebSearchIndexingAPI($url);
+
+            return response()->json(['error' => false, 'message' => 'The URL has been submitted for indexing.', 'indexingResponse' => $indexingResponse]);
+        } else {
+            $errors = [];
             foreach ($data->matches as $match) {
                 $errors[] = $match->threatType;
             }
-        }
 
-        return $errors;
+            return response()->json(['error' => true, 'message' => 'The URL contains the following threats: ' . implode(', ', $errors)]);
+        }
+    }
+
+    private function isEmptyObject($obj)
+    {
+        return json_encode($obj) === '{}';
+    }
+
+    private function submitToWebSearchIndexingAPI($url)
+    {
+        // JSON key path.
+        $keyFilePath = 'konstantindev-315e11644074.json';
+
+        $googleClient = new Google\Client();
+        $googleClient->setAuthConfig($keyFilePath);
+        $googleClient->setScopes(Google_Service_Indexing::INDEXING);
+        $googleIndexingService = new Google_Service_Indexing($googleClient);
+
+        $urlNotification = new Google_Service_Indexing_UrlNotification([
+            'url' => $url,
+            'type' => 'URL_UPDATED'
+        ]);
+
+        try {
+            $result = $googleIndexingService->urlNotifications->publish($urlNotification);   
+
+            return $result;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
